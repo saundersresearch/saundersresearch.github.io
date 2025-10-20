32f1787938cb7186e9c91347f2c576f5c1796a79
saundersresearch <adam.m.saunders@vanderbilt.edu>
2025-10-19 23:13:40 -0500

    Update site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -52,4 +52,31 @@ var store = [{
         "tags": ["Vanderbilt MASI Lab","SPIE Medical Imaging","fMRI"],
         "url": "/posts/vasculature-smoothing/",
         "teaser": null
-      }]
+      },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist.  ","url": "http://localhost:4000/404.html"
+  },{
+    "title": "About",
+    "excerpt":"My name is Adam Saunders. As a graduate student at Vanderbilt University in the MASI Lab, I challenge myself to learn more about the world of medical imaging. My goal is to learn more from often imperfectly-acquired medical images, rooted in the idea that the pixels represent not just a...","url": "http://localhost:4000/about/"
+  },{
+    "title": "Contact Me",
+    "excerpt":"I would love to start a conversation with you! I am always interested in learning more and connecting with new people, so don’t be afraid to reach out. If you’re interested in reaching me, feel free to send an email to me at adam.m.saunders@vanderbilt.edu. I would love to connect with...","url": "http://localhost:4000/contact/"
+  },{
+    "title": "CV",
+    "excerpt":"Summary An electrical engineering PhD student studying medical imaging research focused on applying artificial intelligence tools to impute missing information from imperfect data. Strengths in analytical skills and scientific communication. Contact Information adam.m.saunders@vanderbilt.edu adamsaunders97 Education Vanderbilt University August 2023 - Present PhD, Electrical and Computer Engineering University of Dayton August...","url": "http://localhost:4000/cv/"
+  },{
+    "title": "Home",
+    "excerpt":"About Me I am an electrical and computer engineering PhD student at Vanderbilt University performing medical imaging research in the MASI Lab. Learn more My Research My research focuses on estimating information from MRI where we are missing data we would like to have. Learn more CV Learn more about...","url": "http://localhost:4000/"
+  },{
+    "title": "Posts",
+    "excerpt":" ","url": "http://localhost:4000/posts/"
+  },{
+    "title": "Undergraduate Projects",
+    "excerpt":"Here’s some highlights of cool projects I’ve worked on while a student at the University of Dayton. Diabetic retinopathy and deep learning algorithms Presented as a featured honors thesis at the 2023 Stander Symposium at the University of Dayton Diabetic retinopathy is a disease common in diabetics that can lead...","url": "http://localhost:4000/projects/"
+  },{
+    "title": "Research",
+    "excerpt":"I study medical imaging, with a focus in magnetic resonance images (MRI) in the brain. MRI is an incredibly powerful and flexible imaging modality, allowing us to highlight different features by tuning the parameters under which we acquire the image. Unfortunately, clinically-acquired MRI varies highly based on the site the...","url": "http://localhost:4000/research/"
+  },{
+    "title": "Posts by Tag",
+    "excerpt":" ","url": "http://localhost:4000/tags/"
+  }]
