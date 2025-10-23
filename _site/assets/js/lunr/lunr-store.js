b46ab9d7146c4fc60a82b619189a229c2a1a058c
saundersresearch <adam.m.saunders@vanderbilt.edu>
2025-10-22 23:32:20 -0500

    Change baseurl

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,81 +2,81 @@ var store = [{
         "title": "Using Supercomputers to Detect Cancer: My Summer at Oak Ridge National Lab",
         "excerpt":"I visited Frontier, the world’s fastest supercomputer, as a part of the SULI program this summer How many people can say they got the chance to use one of the fastest computers in the world to detect cancer? I was extremely lucky to be able to do just that this...","categories": [],
         "tags": ["Oak Ridge National Laboratory"],
-        "url": "/pages/saundersresearch/posts/ornl-internship/",
+        "url": "/posts/ornl-internship/",
         "teaser": null
       },{
         "title": "My First Time at SPIE Medical Imaging",
         "excerpt":"I presented at SPIE Medical Imaging for the first time I recently got the chance to travel to San Diego to present research I performed at Oak Ridge National Laboratory at the 2023 SPIE Medical Imaging conference. This was the first professional conference I’ve attended, and getting the chance to...","categories": [],
         "tags": ["Oak Ridge National Laboratory","SPIE Medical Imaging"],
-        "url": "/pages/saundersresearch/posts/spie-medical-imaging-2023/",
+        "url": "/posts/spie-medical-imaging-2023/",
         "teaser": null
       },{
         "title": "I Accepted a PhD Offer at Vanderbilt!",
         "excerpt":"I’m excited to share that I have accepted a PhD offer from Vanderbilt University! I will be joining the Medical-image Analysis and Statistical Interpretation (MASI) Lab under Dr. Bennett Landman. This lab is a part of Vanderbilt Institute for Surgery and Engineering (VISE), an exciting, cross-disciplinary set of labs that...","categories": [],
         "tags": ["Vanderbilt MASI Lab"],
-        "url": "/pages/saundersresearch/posts/phd-acceptance/",
+        "url": "/posts/phd-acceptance/",
         "teaser": null
       },{
         "title": "Graduation from University of Dayton",
         "excerpt":"I celebrated my graduation from the University of Dayton I’m proud to announce I have officially graduated from the University of Dayton! I have graduated with a Bachelor of Electrical Engineering with a minor in Mathematics. It’s been a great few years, and I’m excited for what’s next. I graduated...","categories": [],
         "tags": ["University of Dayton"],
-        "url": "/pages/saundersresearch/posts/ud-graduation/",
+        "url": "/posts/ud-graduation/",
         "teaser": null
       },{
         "title": "Deep Learning for a Healthier World: Detecting and Grading Diabetic Retinopathy",
         "excerpt":"Deep Learning for a Healthier World: Detecting and Grading Diabetic Retinopathy In late April 2023, I presented a talk at the first-ever Honors Thesis Signature Talks at Stander Symposium at the University of Dayton. These talks featured four undergraduate honors researchers presenting their work in a TED-style format. In my...","categories": [],
         "tags": ["University of Dayton"],
-        "url": "/pages/saundersresearch/posts/honors-thesis/",
+        "url": "/posts/honors-thesis/",
         "teaser": null
       },{
         "title": "Super-resolution Multi-Contrast Unbiased Eye Atlases with Deep Probabilistic Refinement",
         "excerpt":"We created standardized reference images for the eye using low-resolution MRI. Ho Hin Lee*, Adam M. Saunders*, Michael E. Kim, Samuel W. Remedios, Lucas W. Remedios, Yucheng Tang, Qi Yang, Xin Yu, Shunxing Bao, Chloe Cho, Louise A. Mawn, Tonia S. Rex, Kevin L. Schey, Blake E. Dewey, Jeffrey M....","categories": [],
         "tags": ["Vanderbilt MASI Lab","Vision"],
-        "url": "/pages/saundersresearch/posts/eye-atlas/",
+        "url": "/posts/eye-atlas/",
         "teaser": null
       },{
         "title": "Comparison and Calibration of MP2RAGE Quantitative T1 Values to Multi-TI Inversion Recovery T1 Values",
         "excerpt":"We found a bias between two methods for mapping quantitative values of T1 from MRI, and we corrected that bias with a patch-based deep learning model. Adam M. Saunders, Michael E. Kim, Chenyu Gao, Lucas W. Remedios, Aravind R. Krishnan, Kurt G. Schilling, Kristin P. O’Grady, Seth A. Smith, and...","categories": [],
         "tags": ["Vanderbilt MASI Lab"],
-        "url": "/pages/saundersresearch/posts/quantitative-t1/",
+        "url": "/posts/quantitative-t1/",
         "teaser": null
       },{
         "title": "A 4D atlas of diffusion-informed spatial smoothing windows for BOLD signal in white matter",
         "excerpt":"Using our atlas, we can adaptively smooth functional MRI data based on white matter tracts, which better preserves the underlying signal present compared to traditional smoothing methods. Adam M. Saunders, Gaurav Rudravaram, Nancy R. Newlin, Michael E. Kim, John C. Gore, Bennett A. Landman, and Yurui Gao. A 4D atlas...","categories": [],
         "tags": ["Vanderbilt MASI","SPIE Medical Imaging","fMRI"],
-        "url": "/pages/saundersresearch/posts/diffusion-smoothing/",
+        "url": "/posts/diffusion-smoothing/",
         "teaser": null
       },{
         "title": "Vasculature-informed spatial smoothing of white matter functional magnetic resonance imaging",
         "excerpt":"With our adaptive vasculature smoothing, we are able to identify more anatomically-informed independent components from fMRI data. Adam M. Saunders, Michael E. Kim, Kurt G. Schilling, John C. Gore, Bennett Papers A. Landman, and Yurui Gao. Vasculature-informed spatial smoothing of white matter functional magnetic resonance imaging. SPIE Medical Imaging: Image...","categories": [],
         "tags": ["Vanderbilt MASI Lab","SPIE Medical Imaging","fMRI"],
-        "url": "/pages/saundersresearch/posts/vasculature-smoothing/",
+        "url": "/posts/vasculature-smoothing/",
         "teaser": null
       },{
     "title": "Page Not Found",
-    "excerpt":"Sorry, but the page you were trying to view does not exist.  ","url": "https://adamsaunders.net/pages/saundersresearch/404.html"
+    "excerpt":"Sorry, but the page you were trying to view does not exist.  ","url": "https://adamsaunders.net/404.html"
   },{
     "title": "About",
-    "excerpt":"My name is Adam Saunders. As a graduate student at Vanderbilt University in the MASI Lab, I challenge myself to learn more about the world of medical imaging. My goal is to learn more from often imperfectly-acquired medical images, rooted in the idea that the pixels represent not just a...","url": "https://adamsaunders.net/pages/saundersresearch/about/"
+    "excerpt":"My name is Adam Saunders. As a graduate student at Vanderbilt University in the MASI Lab, I challenge myself to learn more about the world of medical imaging. My goal is to learn more from often imperfectly-acquired medical images, rooted in the idea that the pixels represent not just a...","url": "https://adamsaunders.net/about/"
   },{
     "title": "Contact Me",
-    "excerpt":"I would love to start a conversation with you! I am always interested in learning more and connecting with new people, so don’t be afraid to reach out. If you’re interested in reaching me, feel free to send an email to me at adam.m.saunders@vanderbilt.edu. I would love to connect with...","url": "https://adamsaunders.net/pages/saundersresearch/contact/"
+    "excerpt":"I would love to start a conversation with you! I am always interested in learning more and connecting with new people, so don’t be afraid to reach out. If you’re interested in reaching me, feel free to send an email to me at adam.m.saunders@vanderbilt.edu. I would love to connect with...","url": "https://adamsaunders.net/contact/"
   },{
     "title": "CV",
-    "excerpt":"Summary An electrical engineering PhD student studying medical imaging research focused on applying artificial intelligence tools to impute missing information from imperfect data. Strengths in analytical skills and scientific communication. Contact Information adam.m.saunders@vanderbilt.edu adamsaunders97 Education Vanderbilt University August 2023 - Present PhD, Electrical and Computer Engineering University of Dayton August...","url": "https://adamsaunders.net/pages/saundersresearch/cv/"
+    "excerpt":"Summary An electrical engineering PhD student studying medical imaging research focused on applying artificial intelligence tools to impute missing information from imperfect data. Strengths in analytical skills and scientific communication. Contact Information adam.m.saunders@vanderbilt.edu adamsaunders97 Education Vanderbilt University August 2023 - Present PhD, Electrical and Computer Engineering University of Dayton August...","url": "https://adamsaunders.net/cv/"
   },{
     "title": "Home",
-    "excerpt":"About Me I am an electrical and computer engineering PhD student at Vanderbilt University performing medical imaging research in the MASI Lab. Learn more My Research My research focuses on estimating information from MRI where we are missing data we would like to have. Learn more CV Learn more about...","url": "https://adamsaunders.net/pages/saundersresearch/"
+    "excerpt":"About Me I am an electrical and computer engineering PhD student at Vanderbilt University performing medical imaging research in the MASI Lab. Learn more My Research My research focuses on estimating information from MRI where we are missing data we would like to have. Learn more CV Learn more about...","url": "https://adamsaunders.net/"
   },{
     "title": "Posts",
-    "excerpt":" ","url": "https://adamsaunders.net/pages/saundersresearch/posts/"
+    "excerpt":" ","url": "https://adamsaunders.net/posts/"
   },{
     "title": "Undergraduate Projects",
-    "excerpt":"Here’s some highlights of cool projects I’ve worked on while a student at the University of Dayton. Diabetic retinopathy and deep learning algorithms Presented as a featured honors thesis at the 2023 Stander Symposium at the University of Dayton Diabetic retinopathy is a disease common in diabetics that can lead...","url": "https://adamsaunders.net/pages/saundersresearch/projects/"
+    "excerpt":"Here’s some highlights of cool projects I’ve worked on while a student at the University of Dayton. Diabetic retinopathy and deep learning algorithms Presented as a featured honors thesis at the 2023 Stander Symposium at the University of Dayton Diabetic retinopathy is a disease common in diabetics that can lead...","url": "https://adamsaunders.net/projects/"
   },{
     "title": "Research",
-    "excerpt":"I study medical imaging, with a focus in magnetic resonance images (MRI) in the brain. MRI is an incredibly powerful and flexible imaging modality, allowing us to highlight different features by tuning the parameters under which we acquire the image. Unfortunately, clinically-acquired MRI varies highly based on the site the...","url": "https://adamsaunders.net/pages/saundersresearch/research/"
+    "excerpt":"I study medical imaging, with a focus in magnetic resonance images (MRI) in the brain. MRI is an incredibly powerful and flexible imaging modality, allowing us to highlight different features by tuning the parameters under which we acquire the image. Unfortunately, clinically-acquired MRI varies highly based on the site the...","url": "https://adamsaunders.net/research/"
   },{
     "title": "Posts by Tag",
-    "excerpt":" ","url": "https://adamsaunders.net/pages/saundersresearch/tags/"
+    "excerpt":" ","url": "https://adamsaunders.net/tags/"
   }]
